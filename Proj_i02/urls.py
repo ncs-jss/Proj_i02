98bcf5dd238bac6ce52d407443e98d9cca5ab7b9
ayukha <ayussh.khare@gmail.com>
2018-07-20 05:32:17 +0530

    Inventory App added

--- Proj_i02/urls.py
+++ Proj_i02/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-# from . import accounts
+
 
 urlpatterns = [
 	url(r'^login/', include("accounts.urls")),
