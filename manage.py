6e026be40c702ae306aabd49e514c09d14586b25
ayukha <ayussh.khare@gmail.com>
2018-08-31 17:55:31 +0530

    manage.py fixed

--- manage.py
+++ manage.py
@@ -4,7 +4,11 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Proj_i02.settings")
-
-    from django.core.management import execute_from_command_line
-
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        raise
     execute_from_command_line(sys.argv)
